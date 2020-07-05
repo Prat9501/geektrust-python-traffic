a1a93c0d2c9d4e6af053548a710aacec25fc975d
Prateek Gupta <prateekg.9501@gmail.com>
2020-07-05 14:20:22 +0530

    reshuffled

---
+++ tests/geektrust_test.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from src.get_vehicles import get_vehicle
+
+
+n_files = len(os.listdir('test_data/input_data'))
+get_vehicle('SUNNY')
+import pdb; pdb.set_trace()
+
+# def test_to_check_output_files():
+
